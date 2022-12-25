e0e5f82f72c2386e99b0e908499e557534794848
Sayfullah <sayfullaheid@gmail.com>
2022-12-25 18:53:55 -0500

    Added education section

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Card, CardContent, Divider } from "@mui/material";
+import CarletonDark from './CarletonDark.png'
+import CarletonLight from './CarletonLight.png'
+import Grid from '@mui/material/Unstable_Grid2'
 
 export default function About(props) {
 
     return (
         <Container>
             <Box sx={{ my: 2 }}>
-                <Typography>ABOUT ME</Typography>
+                <Typography variant="h4">About Me</Typography>
+            </Box>
+            <Box sx={{ my: 2 }}>
+                <Card>
+                    <CardContent>
+                        <Grid container>
+                            <Grid xs={6}>
+                                <Typography variant="h6">Bachelor of Information Technology</Typography>
+                                <Typography variant="overline">Network Technology</Typography>
+                            </Grid>
+                            <Grid xs={6} textAlign='right'>
+                                <img src={props.dark ? CarletonDark : CarletonLight} alt="Carleton University Logo" height={75} />
+                            </Grid>
+                        </Grid>
+                        <Divider sx={{ my: 2 }} />
+                        <Typography><b>Courses:</b> DevOps, Advanced Network Routing, Network Security, Data Structures, Database Concepts and SQL</Typography>
+                        <Typography><b>Expected Graduation:</b> May 2024</Typography>
+                        <Typography><b>GPA:</b> 2.9 / 4.0</Typography>
+                    </CardContent>
+                </Card>
             </Box>
         </Container>
     )
