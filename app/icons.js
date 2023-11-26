7e3f934ff1756bca6f58852ef26e6d49ad70a86b
Sayfullah <sayfullaheid@gmail.com>
2023-11-26 12:43:47 -0500

    Add icons for various technologies

---
+++ app/icons.js
@@ -0,0 +1,61 @@
+import {
+    SiCplusplus, SiNodedotjs, SiOpenjdk, SiJavascript, SiReact,
+    SiDocker, SiNeo4J, SiCisco, SiPandas, SiPython, SiScratch,
+    SiLabview, SiSlack, SiFigma, SiGoogledrive, SiGraphql,
+    SiJunipernetworks, SiKubernetes, SiWindows, SiGit,
+    SiLinux, SiFedora, SiPopos, SiKalilinux, SiRaspberrypi,
+    SiTruenas, SiUbuntu, SiHp, SiBootstrap, SiFirst, SiDevpost, SiMui, SiNokia, SiPhp,
+    SiWireguard
+} from 'react-icons/si'
+import { VscSymbolStructure, VscCircuitBoard } from 'react-icons/vsc'
+import { TbRouter } from 'react-icons/tb'
+import { Diversity3, Diversity2, PermPhoneMsg, HourglassTop, GitHub } from '@mui/icons-material'
+
+const Icons = {
+    "java": <SiOpenjdk />,
+    "c++": <SiCplusplus />,
+    "c/c++": <SiCplusplus />,
+    "nodejs": <SiNodedotjs />,
+    "ongdb": <SiNeo4J />,
+    "javascript": <SiJavascript />,
+    "reactjs": <SiReact />,
+    "docker": <SiDocker />,
+    'cisco ios': <SiCisco />,
+    "pandas": <SiPandas />,
+    "python": <SiPython />,
+    'scratch': <SiScratch />,
+    "labview": <SiLabview />,
+    "slack": <SiSlack />,
+    "figma": <SiFigma />,
+    "data structures": <VscSymbolStructure />,
+    "google docs": <SiGoogledrive />,
+    "graphql": <SiGraphql />,
+    "juniper": <SiJunipernetworks />,
+    "kubernetes": <SiKubernetes />,
+    "windows server": <SiWindows />,
+    "routeros": <TbRouter />,
+    'teamwork': <Diversity3 />,
+    "agile development": <Diversity2 />,
+    "communication": <PermPhoneMsg />,
+    "time management": <HourglassTop />,
+    "git": <SiGit />,
+    "linux": <SiLinux />,
+    "popos": <SiPopos />,
+    "kali": <SiKalilinux />,
+    "fedora": <SiFedora />,
+    "raspberrypi": <SiRaspberrypi />,
+    "truenas": <SiTruenas />,
+    "ubuntu": <SiUbuntu />,
+    "aruba": <SiHp />,
+    "bootstrap": <SiBootstrap />,
+    "first": <SiFirst />,
+    "embedded systems": <VscCircuitBoard />,
+    "devpost": <SiDevpost />,
+    "github": <GitHub />,
+    "material ui": <SiMui />,
+    "nokia": <SiNokia />,
+    "php": <SiPhp />,
+    "wireguard": <SiWireguard />
+}
+
+export default Icons
