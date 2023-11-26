5303522c4fff532b8393fffae64e9e80482b39a1
Sayfullah <sayfullaheid@gmail.com>
2023-11-26 12:44:17 -0500

    Add Experience component and page.js file

---
+++ app/experience/Experience.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Container, Typography, Box, Divider, List, ListItem, Chip, ListItemText, Card, CardContent } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2'
+import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot } from "@mui/lab";
+import TimelineOppositeContent, { timelineOppositeContentClasses } from '@mui/lab/TimelineOppositeContent';
+import Icons from '../icons'
+
+export default function Experience({ experiences }) {
+    return (
+        <Container>
+            <Box sx={{ my: 2 }}>
+                <Typography variant="h4">Experience</Typography>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <Timeline
+                    sx={{
+                        [`& .${timelineOppositeContentClasses.root}`]: {
+                            flex: 0.255,
+                        },
+                    }}
+                >
+                    {experiences.map((info, idx) => {
+                        return (
+                            <TimelineItem key={idx}>
+                                <TimelineOppositeContent color="textSecondary">
+                                    <Typography variant="overline">{info.start.month} {info.start.year} - {info.end.month} {info.end.year}</Typography>
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Card>
+                                        <CardContent>
+                                            <Typography variant="h6">
+                                                {info.role} | {info.company}
+                                            </Typography>
+                                            <Divider />
+                                            <Box sx={{ my: 2 }}>
+                                                <List>
+                                                    {info.notes.map((note, idx) => {
+                                                        return (
+                                                            <ListItem key={idx} sx={{ py: 0 }}>
+                                                                <ListItemText>
+                                                                    {note}
+                                                                </ListItemText>
+                                                            </ListItem>
+                                                        )
+                                                    })}
+                                                </List>
+                                            </Box>
+                                            <Box sx={{ my: 2 }}>
+                                                {info.skills.map((skill) => {
+                                                    return (
+                                                        <Chip icon={Icons[skill.toLowerCase()]} key={skill} label={skill} sx={{ mr: 1, pl: 1, mt: 1 }} color={"primary"} />
+                                                    )
+                                                })}
+                                            </Box>
+                                        </CardContent>
+                                    </Card>
+                                </TimelineContent>
+                            </TimelineItem>
+                        )
+                    })}
+                </Timeline>
+            </Box>
+        </Container>
+    )
+}
---
+++ app/experience/page.js
@@ -0,0 +1,100 @@
+/**
+ * List of experiences as objects
+ * 
+ * Template:
+ * ```js
+ * {
+ *  company: string,
+ *  role: string,
+ *  start: {month: string, year: number},
+ *  end: {month: string, year: number},
+ *  notes: string[],
+ *  skills: string[]
+ * }
+ * ```
+ */
+
+'use client'
+
+import { useEffect, useState } from "react";
+import Experience from "./Experience";
+import ExperienceMobile from "./ExperienceMobile";
+
+const exp = require("constants");
+
+const experiences = [
+    {
+        company: "Sphyrna Security Inc.",
+        role: "Research & Development",
+        start: { month: "May", year: 2023 },
+        end: { month: "August", year: 2023 },
+        notes: [
+            "Explored secure communications methods, such as Wireguard, SSH, and SFTP",
+            "Developed software for a prototype secure communication device",
+            "Researched device and Linux Kernel hardening best practices",
+        ],
+        skills: ["C++", "Linux", "WireGuard"]
+    },
+    {
+        company: "Sphyrna Security Inc.",
+        role: "Web Developer",
+        start: { month: "January", year: 2022 },
+        end: { month: "December", year: 2022 },
+        notes: [
+            "Developed a full stack React/NodeJS solution to load the front-end web application in the user's preferred language, which increased our customer adoption rate as we could deploy to more regions",
+            "Implemented a secure authentication system using JWT, web cookies, and multi-factor authentication",
+            "Followed up on customer reported issues through GitLab"
+        ],
+        skills: ["ReactJS", "NodeJS", "ONgDB", "Bootstrap"]
+    },
+    {
+        company: "Sphyrna Security Inc.",
+        role: "Research & Development",
+        start: { month: "May", year: 2021 },
+        end: { month: "August", year: 2021 },
+        notes: [
+            "Collaborated with colleagues to develop prototype software in C/C++, to determine viability as products",
+            "Conducted research on Python attribute-based encryption implementations for use with our secure network products",
+        ],
+        skills: ["C++", "Python"]
+    },
+    {
+        company: "Wize Computing Academy",
+        role: "Summer Intern",
+        start: { month: "July", year: 2020 },
+        end: { month: "August", year: 2020 },
+        notes: [
+            "Designed lesson plans to teach coding skills for students aged 6 and up",
+        ],
+        skills: ["Google Docs", "Python", "Scratch", "LabView"]
+    },
+];
+
+export default function Page() {
+    const [isDesktop, setIsDesktop] = useState(true);
+
+    useEffect(() => {
+        if (window.innerWidth > 768) {
+            setIsDesktop(true);
+        } else {
+            setIsDesktop(false);
+        }
+
+        const updateMedia = () => {
+            if (window.innerWidth > 768) {
+                setIsDesktop(true);
+            } else {
+                setIsDesktop(false);
+            }
+        };
+
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    }, [])
+
+    return (
+        <div>
+            {isDesktop ? (<Experience experiences={experiences} />) : (<ExperienceMobile experiences={experiences} />)}
+        </div>
+    )
+}
