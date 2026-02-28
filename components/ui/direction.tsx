4c44b4430125a10bafc44221c0a5fdca0183f772
Sayfullah <sayfullaheid@gmail.com>
2026-02-28 16:34:41 -0500

    fix: standardize string quotes in direction.tsx

--- components/ui/direction.tsx
+++ components/ui/direction.tsx
@@ -1,14 +1,14 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { Direction } from "radix-ui"
+import * as React from 'react'
+import { Direction } from 'radix-ui'
 
 function DirectionProvider({
   dir,
   direction,
   children,
 }: React.ComponentProps<typeof Direction.DirectionProvider> & {
-  direction?: React.ComponentProps<typeof Direction.DirectionProvider>["dir"]
+  direction?: React.ComponentProps<typeof Direction.DirectionProvider>['dir']
 }) {
   return (
     <Direction.DirectionProvider dir={direction ?? dir}>
